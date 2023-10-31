c08d1159f9f564a161bca17dbda29c0de98378de
Sava Mikalacki <sava@codekrafters.com>
2023-10-31 10:20:01 +0100

    Update ArielOS hosts

--- generateArielHosts.sh
+++ generateArielHosts.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python3 updateHostsFile.py --noupdate --extensions porn gambling fakenews
+python3 updateHostsFile.py --noupdate --extensions porn gambling fakenews --output arielos_hosts --nogendata
 
